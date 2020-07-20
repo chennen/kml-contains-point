be25f50a895aa980460b0c4fc55ab97ab3d6b41d
chennen <cthennen@gmail.com>
2020-07-19 23:28:24 -0500

    cli working

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import { main } from "./main";
 
 // Should validate these args are correct, not going to for this quick app
-const [_, kmlFilename, testPointX, testPointY] = process.argv;
-
+const [kmlFilename, testPointX, testPointY] = process.argv.slice(2);
 const testPoint = {
-  x: parseInt(testPointX, 10),
-  y: parseInt(testPointY, 10),
+  x: Number.parseFloat(testPointX),
+  y: Number.parseFloat(testPointY),
 };
 
 main(kmlFilename, testPoint);
